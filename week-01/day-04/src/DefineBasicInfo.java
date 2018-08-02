2202d86e1ff0d37f0a10fc0db17b25096ddb3159
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-02 14:40:17 +0200

    basic info provided

---
+++ week-01/day-04/src/DefineBasicInfo.java
@@ -0,0 +1,13 @@
+public class DefineBasicInfo {
+  public static void main(String[] args) {
+    String myName = "Kriszta";
+    int myAge = 36;
+    double myHeight = 1.72;
+    boolean isMarried = false;
+
+    System.out.println(myName);
+    System.out.println(myAge);
+    System.out.println(myHeight);
+    System.out.println(isMarried);
+  }
+}
