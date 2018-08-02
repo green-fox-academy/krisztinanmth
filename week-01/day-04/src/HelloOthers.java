1f176ab9bcc0f08c198cc0114970375cd2b730ac
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-02 13:45:06 +0200

    HelloOthers done

---
+++ week-01/day-04/src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+  public static void main(String[] args) {
+    System.out.println("Hello, Mandana!");
+    System.out.println("Hello, Ben!");
+    System.out.println("Hello, Archi!");
+  }
+}
