6fd585d5fcbca141da57a851ab758e9a7aa57c2e
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-02 17:08:14 +0200

    cuboid finished

--- week-01/day-04/src/Cuboid.java
+++ week-01/day-04/src/Cuboid.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Cuboid {
+  public static void main(String[] args) {
+
+    double a = 20;
+    double b = 34;
+    double c = 67;
+
+    double SurfaceArea = a + b + c;
+    double Volume = a * b * c;
+
+    System.out.println("Surface Area: " + SurfaceArea);
+    System.out.println("Volume: " + Volume);
+
+  }
 }
