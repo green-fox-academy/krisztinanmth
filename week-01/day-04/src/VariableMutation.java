a6fd9c042d8f82eaab0e7892def09d1ba916e972
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-02 16:00:14 +0200

    variableMutation finished

--- week-01/day-04/src/VariableMutation.java
+++ week-01/day-04/src/VariableMutation.java
@@ -1,4 +1,74 @@
-package PACKAGE_NAME;
-
 public class VariableMutation {
+  public static void main(String[] args) {
+    int a = 3;
+    // make it bigger by 10
+    a += 10;
+    System.out.println(a);
+
+
+    int b = 100;
+    // make it smaller by 7
+    b -= 7;
+    System.out.println(b);
+
+
+    int c = 44;
+    // please double c's value
+    c *= 2;
+    System.out.println(c);
+
+
+    int d = 125;
+    // please divide by 5 d's value
+    d /= 5;
+    System.out.println(d);
+
+
+    int e = 8;
+    // please cube of e's value
+    e = (int) Math.pow(e, 3);
+    System.out.println(e);
+
+
+    int f1 = 123;
+    int f2 = 345;
+    // tell if f1 is bigger than f2 (print as a boolean)
+    System.out.println(f1 > f2);
+
+
+    int g1 = 350;
+    int g2 = 200;
+    // tell if the double of g2 is bigger than g1 (print as a boolean)
+    System.out.println((g2 * 2) > g1);
+
+
+    int h = 135798745;
+    // tell if it has 11 as a divisor (print as a boolean)
+    boolean isDivisor = h % 11 == 0;
+    System.out.println(isDivisor);
+
+
+    int i1 = 10;
+    int i2 = 3;
+    // tell if i1 is higher than i2 squared and smaller than i2 cubed (print as a boolean)
+    int square = i2 * i2;
+    int cube = square * i2;
+    boolean squaredCubed = ((i1 > square) && (i1 < cube));
+    System.out.println(squaredCubed);
+
+
+    int j = 1521;
+    // tell if j is dividable by 3 or 5 (print as a boolean)
+    boolean dividable = ((j % 3 == 0) || (j % 5 == 0));
+    System.out.println(dividable);
+
+
+    String k = "Apple";
+    //fill the k variable with its cotnent 4 times
+    String l = "";
+    for (int n = 0; n < 4; n++) {
+      l += k;
+    }
+    System.out.println(l);
+  }
 }
