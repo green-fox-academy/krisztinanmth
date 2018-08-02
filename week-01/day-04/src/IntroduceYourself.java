a25d09b8e9b9718926fee5cf45f13ecbc2fd8dc9
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-02 13:54:30 +0200

    introduceYourself finished

---
+++ week-01/day-04/src/IntroduceYourself.java
@@ -0,0 +1,7 @@
+public class IntroduceYourself {
+  public static void main(String[] args) {
+    System.out.println("Krisztina Nemeth");
+    System.out.println(36);
+    System.out.println(1.72);
+  }
+}
