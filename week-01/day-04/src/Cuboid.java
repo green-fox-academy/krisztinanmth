e7290b5cf6a99015c55ec8d568088f1fd5a1bc5a
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-02 20:01:36 +0200

    corrected version, please ignore former one

--- week-01/day-04/src/Cuboid.java
+++ week-01/day-04/src/Cuboid.java
@@ -5,7 +5,7 @@ public class Cuboid {
     double b = 34;
     double c = 67;
 
-    double SurfaceArea = a + b + c;
+    double SurfaceArea = (a * b + a * c + b * c) * 2;
     double Volume = a * b * c;
 
     System.out.println("Surface Area: " + SurfaceArea);
