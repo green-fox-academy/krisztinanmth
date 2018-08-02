ddd3a014903b3574d3ecb49a9ba22bd0b57bbade
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-02 17:35:36 +0200

    OddEven finished

---
+++ week-01/day-04/src/OddEven.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class OddEven {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Please enter a whole number");
+
+    int myNumber = input.nextInt();
+
+    if (myNumber % 2 == 0) {
+      System.out.println("Even");
+    }
+    else {
+      System.out.println("Odd");
+    }
+  }
+}
