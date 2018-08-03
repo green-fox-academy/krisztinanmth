ef2a6d119e3994ab03af4af9a40e869a41dfc66c
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-03 14:24:23 +0200

    finished

---
+++ week-01/day-04/src/IWontCheatOnTheExams.java
@@ -0,0 +1,12 @@
+public class IWontCheatOnTheExams {
+  public static void main(String[] args) {
+
+    String a = "I won't cheat on the exams!";
+    String l = "";
+
+    for (int n = 0; n < 100; n++) {
+      l += a;
+      System.out.println(a);
+    }
+  }
+}
