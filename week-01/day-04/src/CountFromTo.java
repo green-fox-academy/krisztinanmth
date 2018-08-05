645a2cd0819c24e8cabcd5fd7175260dc7798943
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-05 18:58:24 +0200

    CountFromTo finished

--- week-01/day-04/src/CountFromTo.java
+++ week-01/day-04/src/CountFromTo.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class CountFromTo {
+  public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Please enter a whole number.");
+    int smallNumber = input.nextInt();
+
+    System.out.println("Please enter a bigger number.");
+    int bigNumber = input.nextInt();
+
+    if (bigNumber <= smallNumber) {
+      System.out.println("Please enter a bigger number than your first number. Thank you.");
+    }
+    else for (int i = smallNumber; i <= bigNumber; i++) {
+      System.out.println(i);
+    }
+  }
 }
