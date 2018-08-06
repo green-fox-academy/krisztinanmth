565d43e97c8c3f5912b136749b3e1c5277bf2858
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-06 13:37:51 +0200

    greet java finished with one add. variable

--- week-02/day-01/src/greet.java
+++ week-02/day-01/src/greet.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+//  From now on, create the usual class wrapper
+//  and main method on your own.
+
+// - Create a string variable named `al` and assign the value `Greenfox` to it
+// - Create a function called `greet` that greets it's input parameter
+//     - Greeting is printing e.g. `Greetings dear, Greenfox`
+// - Greet `al`
 
 public class greet {
+  public static void main(String[] args) {
+    String al = "Greenfox";
+    String user = "Krisztina";
+    System.out.println(greeting(al));
+    System.out.println(greeting(user));
+  }
+
+  public static String greeting(String al) {
+    String result = "Greetings dear, " + al;
+    return result;
+  }
 }
