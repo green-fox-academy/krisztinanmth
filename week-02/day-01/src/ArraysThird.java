d9a3c44fc227463269327aa83e3b1f45bee360c0
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-06 15:33:59 +0200

    ArraysThird done

---
+++ week-02/day-01/src/ArraysThird.java
@@ -0,0 +1,16 @@
+// - Create an array variable named `q`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print the third element of `q`
+
+public class ArraysThird {
+  public static void main(String[] args) {
+    int q[] = {4, 5, 6, 7};
+    System.out.println("the third element is: " + q[2]);
+  }
+}
+
+//int q[] = new int[4];
+// q[0] = 4;
+// q[1] = 5;
+// q[2] = 6;
+// q[3] = 7;
