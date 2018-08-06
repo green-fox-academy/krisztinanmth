7e57f87bc5f2f1808e8354dd1f7e055cbcde098c
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-06 13:13:38 +0200

    doubling finished

---
+++ week-02/day-01/src/Doubling.java
@@ -0,0 +1,11 @@
+public class Doubling {
+  public static void main(String[] args) {
+    int baseNum = 123;
+    System.out.println(doublingFunction(+baseNum));
+  }
+
+  public static int doublingFunction(int baseNum) {
+    int result = baseNum * 2;
+    return result;
+  }
+}
