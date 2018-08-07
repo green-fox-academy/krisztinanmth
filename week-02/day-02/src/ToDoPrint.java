802e8d801d4d8319e4d46c94a3ebaed4b8f2a246
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-07 14:57:23 +0200

    ToDoPrint done

---
+++ week-02/day-02/src/ToDoPrint.java
@@ -0,0 +1,9 @@
+public class ToDoPrint {
+
+  public static void main(String... args){
+
+    String todoText = "My todo: \n" + " - Buy milk\n" + " - Buy milk \n" + " - Download games \n" + "\t - Diablo";
+
+    System.out.println(todoText);
+  }
+}
