728144672b608450d1d6d0a7a813711ccf9a5eff
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-07 16:51:17 +0200

    5

--- week-02/day-02/src/Reverse.java
+++ week-02/day-02/src/Reverse.java
@@ -1,14 +1,22 @@
+import java.util.*;
+
 public class Reverse {
 
   public static void main(String[] args) {
 
     String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
 
-    String reverse = " ";
-
-    for (int i = reversed.length() - 1; i >= 0; i--) {
-      reverse = reverse + reversed.charAt(i);
+    System.out.println(reverse(reversed));
     }
-    System.out.println(reverse);
+
+    public static String reverse(String planets) {
+
+      String result = " ";
+
+      for (int i = planets.length() - 1; i >= 0; i--) {
+        result += planets.charAt(i);
+      }
+      return result;
+    }
   }
-}
+
---
+++ week-02/day-02/src/SolarSystem.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class SolarSystem {
+  public static void main(String... args) {
+
+    ArrayList<String> planetList = new ArrayList<String>(Arrays.asList("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune"));
+
+    System.out.println(putSaturn(planetList));
+  }
+    // Saturn is missing from the planetList
+    // Insert it into the correct position
+    // Create a method called putSaturn() which has list parameter and returns the correct list.
+
+
+    public static String putSaturn(ArrayList<String> planetList) {
+
+      String out = "";
+      planetList.add(planetList.size(), "Saturn");
+      for (int i = 1; i < planetList.size(); i++ ) {
+        out += "\"" + planetList.get(i) + "\"" + " ";
+      }
+      return out;
+
+
+    // Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Saturn"
+  }
+}
