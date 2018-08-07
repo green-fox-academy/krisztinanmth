168ffe35752c0d3b89c97e3d6372f1792f8de89a
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-07 06:59:29 +0200

    colors done

---
+++ week-02/day-01/src/mainClassHomework.java
@@ -0,0 +1,17 @@
+package javaisepic;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class mainClassHomework {
+  public static void main(String[] args) {
+    String s = "word";
+    String s1 = "WORDIERWORD";
+
+    System.out.println(s.toUpperCase());
+    System.out.println(s1.toLowerCase());
+
+    if (s1.startsWith("word")) {
+      System.out.println("s1 starts with word LOLOWOLE");
+    }
+  }
+}
