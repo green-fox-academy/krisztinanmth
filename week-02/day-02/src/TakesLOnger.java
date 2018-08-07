58a36e6cd2931a192799adf56f5e52c71eb4c14a
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-07 14:46:22 +0200

    TakesLonger - done 3 methods

--- week-02/day-02/src/TakesLOnger.java
+++ week-02/day-02/src/TakesLOnger.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+
 
 public class TakesLOnger {
+
+  public static void main(String... args) {
+    String quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
+
+    // method 1 - with substring
+    System.out.println(quote.indexOf("It "));
+    String newquote;
+    newquote = quote.substring(0,21) + "always takes longer than " + quote.substring(21);
+    System.out.println(newquote);
+
+    //method 2 - with substring
+    System.out.println(quote.substring(0, quote.indexOf("you")) + "always takes longer than " + (quote.substring(quote.indexOf("you"))));
+
+    //method 2 - with StringBuilder
+    StringBuilder newString = new StringBuilder(quote);
+    newString.insert(quote.indexOf("you"), "always takes longer than ");
+
+    System.out.println(newString);
+  }
 }
