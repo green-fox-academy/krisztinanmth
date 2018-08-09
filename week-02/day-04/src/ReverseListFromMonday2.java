84f1cfa7389c9243305a22c95673770bfb57a856
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-09 13:54:36 +0200

    this is done with arrayList so no good again

--- week-02/day-04/src/ReverseListFromMonday2.java
+++ week-02/day-04/src/ReverseListFromMonday2.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class ReverseListFromMonday2 {
+
+  public static void main(String[] args) {
+
+    ArrayList<Integer> aj = new ArrayList<>();
+    aj.add(3);
+    aj.add(4);
+    aj.add(5);
+    aj.add(6);
+    aj.add(7);
+
+    System.out.println("Numbers before being reversed: " + aj);
+    Collections.reverse(aj);
+    System.out.println("Numbers after being reversed: " + aj);
+  }
 }
