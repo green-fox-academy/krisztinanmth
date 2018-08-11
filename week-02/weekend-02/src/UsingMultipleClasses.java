0265709cb40813bbce23283a9c7decb247145b6f
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-11 11:14:12 +0200

    bucky using multiple classes

---
+++ week-02/weekend-02/src/UsingMultipleClasses.java
@@ -0,0 +1,8 @@
+public class UsingMultipleClasses {
+  public static void main(String[] args) {
+    tunaBucky tunaObject = new tunaBucky(); //this is an object we created to use anything that's in tunaBucky class
+    tunaObject.simpleMeassage();            //this is the line where we tell what we want it to do with .simpleMessage method
+
+    //so if we run the program from this windom it displays "This is another class called tunaBucky" for us
+  }
+}
---
+++ week-02/weekend-02/src/tunaBucky.java
@@ -0,0 +1,11 @@
+public class tunaBucky {
+  public void simpleMeassage(){
+    System.out.println("This is another class called tunaBucky");
+  }
+  //public means anyone can use it
+  //void means it is going to do something
+
+  //if I want to run this class from here to output the shout ... it is not going to happen as Java
+  // just runs from the Main Class!
+
+}
