506516904b8b6cc8441ec613176cdbba622b8816
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-12 13:01:51 +0200

    bucky simple averaging program

--- week-02/weekend-02/src/BSimpleAveragingProgram.java
+++ week-02/weekend-02/src/BSimpleAveragingProgram.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class BSimpleAveragingProgram {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int total = 0; //holds the sum of all the grades
+    int grade;
+    int average;
+    int counter = 0;
+    while (counter < 10) {
+      grade = input.nextInt();
+      total = total + grade;
+      counter++;
+    }
+    average = total/10; //we divide it by 10 as we put in 10 numbers
+    System.out.println("Your average is: " + average);
+  }
 }
