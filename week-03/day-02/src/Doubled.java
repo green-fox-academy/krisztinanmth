bf774395c8d4bbd754030c70743db1659968d070
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-14 19:55:58 +0200

    Logs started

---
+++ week-03/day-02/src/Doubled.java
@@ -0,0 +1,13 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Doubled {
+  public static void main(String[] args) {
+    //create a method that decrypts the duplicated-chars.txt
+    Path path = Paths.get("assets/duplicated-chars.txt");
+  }
+
+  public static void decryptTxt(Path path) {
+    
+  }
+}
--- week-03/day-02/src/Logs.java
+++ week-03/day-02/src/Logs.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import javafx.scene.shape.Line;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Logs {
+  public static void main(String[] args) {
+    // Read all data from 'log.txt'.
+    // Each line represents a log message from a web server
+    // Write a function that returns an array with the unique IP addresses.
+    // Write a function that returns the GET / POST request ratio.
+    Path path = Paths.get("assets/log.txt");
+    List<String> unique = new ArrayList<>();
+  }
+//  public static ArrayList uniqueIPs(Path path, List unique) {
+//    try {
+//      for (String line : Files.readAllLines(path)) {
+//      }
+//    } catch (IOException e) {
+//      System.out.println("the file can't be found");
+//    }
+//  } return;
 }
