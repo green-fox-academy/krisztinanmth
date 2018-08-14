9e39ca67b5d32c1963881c715866689b1dc9aa3c
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-14 11:56:53 +0200

    Divide by zero completed with function

--- week-03/day-02/src/DivideByZeroWithFunction.java
+++ week-03/day-02/src/DivideByZeroWithFunction.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class DivideByZeroWithFunction {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("please enter a divisor number: ");
+    int divisor = scanner.nextInt();
+    divider(divisor);
+  }
+
+  public static void divider(int divisor) {
+    int result = 0;
+    try {
+      result = 10 / divisor;
+      System.out.println(result);
+    } catch (ArithmeticException e) {
+      System.out.println("cannot divide by zero");
+    }
+  }
 }
