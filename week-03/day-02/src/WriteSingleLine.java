ffdd49db192e267e9ed93328a34a74fc0726da34
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-14 15:32:24 +0200

    write singleLine updated now it adds and not overwrites

--- week-03/day-02/src/WriteSingleLine.java
+++ week-03/day-02/src/WriteSingleLine.java
@@ -1,6 +1,7 @@
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,12 +12,13 @@ public class WriteSingleLine {
     // If the program is unable to write the file,
     // then it should print an error message like: "Unable to write file: my-file.txt"
     List<String> myContent = new ArrayList<>();
-    myContent.add("Krisztina Nemeth");
+    myContent.add("this should be the third line");
     try {// Required by Files.write(filePath, content)
       // Creates a new file if not exists and overwrites it's content
       // The elements of the content lists will become the lines of the file
       Path myPath = Paths.get("assets/my-file.txt");
-      Files.write(myPath, myContent);
+      Files.write(myPath, myContent, StandardOpenOption.APPEND);
+      //you need to put StandardOpenOption.APPEND here otherwise it just overwrites the file and not adds
       for (String line : Files.readAllLines(myPath)) {
         System.out.println(line);
       }
