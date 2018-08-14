c09c51796277c47b3ba3e289ca6a7b8ff4aa0013
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-14 11:33:46 +0200

    Print Each Line finished

--- week-03/day-02/src/PrintEachLine.java
+++ week-03/day-02/src/PrintEachLine.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 
 public class PrintEachLine {
+  public static void main(String[] args) {
+    // Write a program that opens a file called "my-file.txt", then prints
+    // each of the lines form the file.
+    // If the program is unable to read the file (for example it does not exists),
+    // then it should print an error message like: "Unable to read file: my-file.txt"
+    Path path = Paths.get("my-file.txt");
+    try {
+      for(String line : Files.readAllLines(path)) {
+        System.out.println(line);
+      }
+    } catch (IOException e) {
+      System.out.println("Unable to read file: my-file.txt");
+    }
+  }
 }
+
