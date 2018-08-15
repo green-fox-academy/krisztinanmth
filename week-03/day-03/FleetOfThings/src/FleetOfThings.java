2f97de90dae603caed80df2326d64522a7294a25
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-15 22:00:23 +0200

    FleetOfThings finished

--- week-03/day-03/FleetOfThings/src/FleetOfThings.java
+++ week-03/day-03/FleetOfThings/src/FleetOfThings.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class FleetOfThings {
+  public static void main(String[] args) {
+    Fleet fleet = new Fleet();
+    // Create a fleet of things to have this output:
+    // 1. [ ] Get milk
+    // 2. [ ] Remove the obstacles
+    // 3. [x] Stand up
+    // 4. [x] Eat lunch
+    Thing getMilk = new Thing("Get milk");
+    Thing removeTObs = new Thing("Remove the obstacles");
+    Thing standUp = new Thing("Stand up");
+    standUp.complete();
+    Thing eatLunch = new Thing("Eat Lunch");
+    eatLunch.complete();
+
+    fleet.add(getMilk);
+    fleet.add(removeTObs);
+    fleet.add(standUp);
+    fleet.add(eatLunch);
+    System.out.println(fleet);
+  }
 }
--- week-03/day-03/FleetOfThings/src/Thing.java
+++ week-03/day-03/FleetOfThings/src/Thing.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
+/**
+ * Created by aze on 2017.03.29..
+ */
 public class Thing {
+  private String name;
+  private boolean completed;
+
+  public Thing(String name) {
+    this.name = name;
+  }
+
+  public void complete() {
+    this.completed = true;
+  }
+
+  @Override
+  public String toString() {
+    return (completed ? "[x] " : "[ ] ") + name;
+  }
 }
