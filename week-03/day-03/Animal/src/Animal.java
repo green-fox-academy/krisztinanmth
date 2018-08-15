82ded89d749dd62c0016bd6b8df1ab8e43e3cb09
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-15 21:47:30 +0200

    Animal finished

--- week-03/day-03/Animal/src/Animal.java
+++ week-03/day-03/Animal/src/Animal.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
 public class Animal {
+  private int hunger;
+  private int thirst;
+
+  public Animal(){
+    hunger = 50;
+    thirst = 50;
+  }
+
+  public int hunger() {
+    return hunger;
+  }
+
+  public int thirst() {
+    return thirst;
+  }
+
+  public int eat() {
+    hunger = hunger -1;
+    return hunger;
+  }
+
+  public int drink() {
+    thirst = thirst - 1;
+    return thirst;
+  }
+  public int play() {
+    thirst = thirst + 1;
+    hunger = hunger + 1;
+    return thirst + hunger;
+  }
 }
--- week-03/day-03/Animal/src/AnimalMain.java
+++ week-03/day-03/Animal/src/AnimalMain.java
@@ -1,5 +1,11 @@
 public class AnimalMain {
   public static void main(String[] args) {
-    
+    Animal cat = new Animal();
+
+    System.out.print(cat.thirst() + " \t ");
+    System.out.print(cat.hunger() + " \t ");
+    System.out.print(cat.drink()  + " \t ");
+    System.out.print(cat.eat()    + " \t ");
+    System.out.print(cat.play()   + " \t ");
   }
 }
