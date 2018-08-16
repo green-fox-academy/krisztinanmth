54dedb9082618adc1d73bcc6e54325666361d15c
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-16 12:02:27 +0200

    Power finished

--- week-03/day-04/src/Power.java
+++ week-03/day-04/src/Power.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+// Given base and n that are both 1 or more, compute recursively (no loops)
+// the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
 
 public class Power {
+  public static void main(String[] args) {
+    System.out.println(powerWithRecursion(2, 3));
+  }
+  public static int powerWithRecursion(int base, int n) {
+    if(base < 1 || n < 1) {
+      return 1;
+    } else {
+      return base * powerWithRecursion(base, n - 1);
+    }
+  }
 }
