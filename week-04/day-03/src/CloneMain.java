60c4cf024c293cca8b8519fda8955923eb8015b7
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-22 14:17:14 +0200

    Clone finished

--- week-04/day-03/src/CloneMain.java
+++ week-04/day-03/src/CloneMain.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class CloneMain {
+  public static void main(String[] args) {
+    Student johnny = new Student("John", 20, "male", "BME");
+    Student johnnyClone = johnny.clone();
+    System.out.println(johnnyClone.name);
+    System.out.println(johnny.name);
+    System.out.println(johnnyClone);
+    System.out.println(johnny);
+    Student kriszta = new Student("krisz", 21, "female", "null");
+    Student krisztaClone = kriszta.clone();
+    System.out.println(kriszta);
+    System.out.println(krisztaClone);
+  }
 }
--- week-04/day-03/src/Student.java
+++ week-04/day-03/src/Student.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+public class Student implements Cloneable{
+  String previousOrganization;
+  int skippedDays;
+  String name;
+  int age;
+  String gender;
 
-public class Student {
+  public Student(String name, int age, String gender, String previousOrganization) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.previousOrganization = previousOrganization;
+    skippedDays = 0;
+  }
+
+  public Student clone() {
+    Student john = new Student(name, age, gender, previousOrganization);
+    return john;
+  }
+
+  public Student() {
+    name = "Jane Doe";
+    age = 30;
+    gender = "female";
+    previousOrganization = "The School of Life";
+    skippedDays = 0;
+  }
+
+  public void getGoal() {
+    System.out.println("Be a junior software developer");
+  }
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + name + ", a " + age + " year old " + gender + " from " + previousOrganization + " who skipped " + skippedDays + " days from the course already.");
+  }
+
+  public int skipDays(int numberOfDays) {
+    skippedDays += numberOfDays;
+    return skippedDays;
+  }
 }
