46f8358cfa4bb4c33fa41f44a69edba2cd12f70e
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-22 14:26:48 +0200

    bunch deleted CharAtSequence started

---
+++ week-04/day-02/CharAtSequence/src/Gnirts.java
@@ -0,0 +1,22 @@
+public class Gnirts implements CharSequence {
+  String str;
+
+  public Gnirts(String str) {
+    this.str = str;
+  }
+
+  @Override
+  public int length() {
+    return 0;
+  }
+
+  @Override
+  public char charAt(int index) {
+    return 0;
+  }
+
+  @Override
+  public CharSequence subSequence(int start, int end) {
+    return null;
+  }
+}
