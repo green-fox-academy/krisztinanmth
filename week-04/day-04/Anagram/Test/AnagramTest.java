b95b69f1dd2c36560a70973419286eb0c573eb25
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-23 16:53:58 +0200

    Sum started

--- week-04/day-04/Anagram/Test/AnagramTest.java
+++ week-04/day-04/Anagram/Test/AnagramTest.java
@@ -1,5 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class AnagramTest {
 
+  @Test
+  public void testingIsAnagramMethod() {
+    isAnagram anagram = new isAnagram();
+    Assert.assertEquals(false, );
+  }
 }
--- week-04/day-04/Anagram/src/isAnagram.java
+++ week-04/day-04/Anagram/src/isAnagram.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class isAnagram {
+  public static void main(String[] args) {
+    String str1 = "dog";
+    String str2 = "god";
+    System.out.println(isAnagram(str1, str2));
+  }
+
+  public static boolean isAnagram(String str1, String str2) {
+    if(str1.length() != str2.length()) {
+      return false;
+    }
+    char[] arr1 = str1.toCharArray();
+    char[] arr2 = str2.toCharArray();
+    Arrays.sort(arr1);
+    Arrays.sort(arr2);
+    if(arr1.equals(arr2)) {
+      return true;
+    } else {
+      return false;
+    }
+
+
+//  public static boolean isAnagram(String str1, String str2) {
+//    char[] arr1 = str1.toCharArray();
+//    char[] arr2 = str2.toCharArray();
+//    if (str1.length() == str2.length()) {
+//      Arrays.sort(arr1);
+//      Arrays.sort(arr2);
+//    } if (arr1.equals(arr2)) {
+//        return true;
+//      } else {
+//        return false;
+//      }
+  }
 }
