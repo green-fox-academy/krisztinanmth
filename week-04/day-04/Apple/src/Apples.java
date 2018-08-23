10421247d333febbee67c29914788919883d4771
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-23 14:56:33 +0200

    Apples done

--- week-04/day-04/Apple/src/Apples.java
+++ week-04/day-04/Apple/src/Apples.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Apples {
+
+  String str = "apple";
+
+  public String getApple() {
+    return str;
+  }
 }
---
+++ week-04/day-04/Apple/test/ApplesTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApplesTest {
+
+
+  @Test
+  public void testPrintMessage() {
+    Apples ap = new Apples();
+    Assert.assertEquals("apple", ap.getApple());
+  }
+
+}
