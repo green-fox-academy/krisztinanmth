21ce40a91c00f273a633b6d901e370a30d208d83
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-23 16:53:33 +0200

    Sum started

---
+++ week-04/day-04/Sum/Test/SumTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SumTest {
+
+  @Test
+  public void IsSumWorking() {
+    Sum sum = new Sum();
+  }
+}
---
+++ week-04/day-04/Sum/src/Sum.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sum {
+  List<Integer> listOfNumbers;
+
+  public Sum() {
+    listOfNumbers = new ArrayList<>();
+  }
+
+  public void add(int number) {
+    listOfNumbers.add(number);
+  }
+
+  public int sum(ArrayList<Integer> listOfNumbers) {
+    int sum = 0;
+    for (int i = 0; i < listOfNumbers.size() ; i++) {
+    }
+    return sum;
+  }
+}
