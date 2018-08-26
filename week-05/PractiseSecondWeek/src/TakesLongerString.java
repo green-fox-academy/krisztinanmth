ddbb7cec66b2e663a9e945d59fb88e260e9c0fbf
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-26 10:25:51 +0200

    takes longer finished with substrings

---
+++ week-05/PractiseSecondWeek/src/TakesLongerString.java
@@ -0,0 +1,12 @@
+public class TakesLongerString {
+  public static void main(String... args){
+    String quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
+    // When saving this quote a disk error has occured. Please fix it.
+    // Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
+    // Using pieces of the quote variable (instead of just redefining the string)
+    String first = quote.substring(0, 20);
+    String middle = " always takes longer than ";
+    String end = quote.substring(21);
+    System.out.println(first + middle + end);
+  }
+}
