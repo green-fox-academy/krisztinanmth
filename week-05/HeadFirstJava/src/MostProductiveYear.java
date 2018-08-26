648c3f0076a1144f1fa5efed3d4a946088122639
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-26 21:15:11 +0200

    files practise started

---
+++ week-05/HeadFirstJava/src/MostProductiveYear.java
@@ -0,0 +1,21 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MostProductiveYear {
+  public static void main(String[] args) {
+    //nameOfFilmStudio - is the path for files
+    //either "assets/marvel.csv"
+    //or "assets/paramount.csv"
+    String nameOfFilmStudio = null;
+    Path path = Paths.get(nameOfFilmStudio);
+    //List<String> line = new ArrayList<>();
+  }
+
+//  public static String mostProductiveYear(String nameOfFilmStudio) {
+//    try {
+//      for (String line)
+//    }
+//  }
+}
