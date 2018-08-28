d2ef07fb0af6b831b7eb1628f0941297612274ed
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-28 07:39:20 +0200

    most productive year, not ready yet

--- week-05/HeadFirstJava/src/MostProductiveYear.java
+++ week-05/HeadFirstJava/src/MostProductiveYear.java
@@ -1,21 +1,49 @@
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MostProductiveYear {
   public static void main(String[] args) {
     //nameOfFilmStudio - is the path for files
     //either "assets/marvel.csv"
     //or "assets/paramount.csv"
-    String nameOfFilmStudio = null;
-    Path path = Paths.get(nameOfFilmStudio);
-    //List<String> line = new ArrayList<>();
+    String path1 = "assets/paramount";
+    String path2 = "assets/marvel";
+    String name1 = "paramount";
+    String name2 = "marvel";
+    String name3 = "ghibli";
+    String path3 = "assets/ghibli";
+    System.out.println(mostProductiveYear(name1, path1));
+    System.out.println(mostProductiveYear(name2, path2));
+    System.out.println(mostProductiveYear(name3, path3));
+
+
   }
 
-//  public static String mostProductiveYear(String nameOfFilmStudio) {
-//    try {
-//      for (String line)
-//    }
-//  }
+  static String mostProductiveYear(String filename, String path) {
+    List<String> linesOfInputFile = null;
+    ArrayList<String> yearList = new ArrayList<>();
+    try {
+      linesOfInputFile = Files.readAllLines(Paths.get(path + ".csv"));
+    } catch (IOException e) {
+      return "Cannot find studio, please try again later.";
+    }
+    Map<String, Integer> map = new HashMap<>();
+    String year = null;
+    for (String line : linesOfInputFile) {
+      year = line.split(",")[1];
+      if (map.get(year) == null) {
+        map.put(year, 1);
+      } else {
+        map.put(year, map.get(year) + 1);
+      }
+    }
+    
+    return "The " + filename + " has made the most movies in " + stringOfLargest;
+  }
 }
