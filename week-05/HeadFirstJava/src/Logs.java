ef7efa45aa2a29472a2acffcc7c38907bbfa5104
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-29 21:55:56 +0200

    Logs - unique IP addresses found

--- week-05/HeadFirstJava/src/Logs.java
+++ week-05/HeadFirstJava/src/Logs.java
@@ -11,15 +11,14 @@ public class Logs {
     // Each line represents a log message from a web server
     // Write a function that returns an array with the unique IP addresses.
     // Write a function that returns the GET / POST request ratio.
+    System.out.println(findIPs());
 
-
-    //System.out.println(uniqIP());
   }
-  List<String> lines = new ArrayList<>();
 
-  public static String findIPs() {
+  public static List<String> findIPs() {
     List<String> lines = new ArrayList<>();
-    String IPAddress = "";
+    String ipAddress;
+    List<String> uniqueIPAddress = new ArrayList<>();
     HashMap<String, Integer> uniqueIPs = new HashMap<>();
     try {
       Path filePath = Paths.get("assets/log.txt");
@@ -27,32 +26,20 @@ public class Logs {
     } catch (Exception e) {
       System.out.println("file is not found or not readable, sorry");
     }
-    return null;
-  }
-
-    public static ArrayList uniqIP(List<String> lines) {
-      ArrayList<String> IPs = new ArrayList<>();
-      for (int i = 0; i < lines.size(); i++) {
-        String IP = lines.get(i).split("   ")[1];
-        if (!IPs.contains(IP)) {
-          IPs.add(IP);
-        }
+    for (int i = 0; i < lines.size(); i++) {
+      ipAddress = lines.get(i).split("   ")[1];
+      if (!uniqueIPs.containsKey(ipAddress)) {
+        uniqueIPs.put(ipAddress, 1);
+      } else {
+        //Integer temp = uniqueIPs.get(IPAddress);
+        uniqueIPs.put(ipAddress, uniqueIPs.get(ipAddress) + 1);
       }
-      return IPs;
     }
-//    for (int i = 0; i < lines.size() ; i++) {
-//      IPAddress = lines.get(i).split("   ")[1];
-//      if (!uniqueIPs.containsKey(IPAddress)) {
-//        uniqueIPs.put(IPAddress, 1);
-//      } else {
-//        //Integer temp = uniqueIPs.get(IPAddress);
-//        uniqueIPs.put(IPAddress, uniqueIPs.get(IPAddress) + 1);
-//      }
-//    }
-//    for (String everyUniqueIp : uniqueIPs.keySet()) {
-//      IPAddress = everyUniqueIp;
-//
-//    }
-//    return IPAddress;
-//  }
+    for (String key : uniqueIPs.keySet()) {
+      if ((uniqueIPs.get(key) == 1)) {
+        uniqueIPAddress.add(key);
+      }
+    }
+    return uniqueIPAddress;
+  }
 }
--- week-05/HeadFirstJava/src/Lollipop.java
+++
@@ -1,8 +0,0 @@
-
-public class Lollipop extends Sweets {
-
-  public Lollipop() {
-    super(10, 5);
-  }
-}
-
