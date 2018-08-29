22bb60c93143822e846d13c7cab093887d31a010
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-29 20:31:16 +0200

    logs some progress, not finished yet...kind of got lost

--- week-05/HeadFirstJava/src/Logs.java
+++ week-05/HeadFirstJava/src/Logs.java
@@ -9,27 +9,50 @@ public class Logs {
   public static void main(String[] args) {
     // Read all data from 'log.txt'.
     // Each line represents a log message from a web server
-    // Write a function that returns an array with the unique IP adresses.
+    // Write a function that returns an array with the unique IP addresses.
     // Write a function that returns the GET / POST request ratio.
-    System.out.println(findIPs());
+
+
+    //System.out.println(uniqIP());
   }
+  List<String> lines = new ArrayList<>();
 
   public static String findIPs() {
-    List<String> lines;
+    List<String> lines = new ArrayList<>();
     String IPAddress = "";
-    int myIndex;
     HashMap<String, Integer> uniqueIPs = new HashMap<>();
     try {
       Path filePath = Paths.get("assets/log.txt");
       lines = Files.readAllLines(filePath);
-      for (int i = 0; i < lines.size() ; i++) {
-        myIndex = lines.get(i).indexOf("   ");
-        IPAddress = lines.get(i).substring(myIndex, myIndex + 14);
-      }
     } catch (Exception e) {
       System.out.println("file is not found or not readable, sorry");
     }
-
-    return IPAddress;
+    return null;
   }
+
+    public static ArrayList uniqIP(List<String> lines) {
+      ArrayList<String> IPs = new ArrayList<>();
+      for (int i = 0; i < lines.size(); i++) {
+        String IP = lines.get(i).split("   ")[1];
+        if (!IPs.contains(IP)) {
+          IPs.add(IP);
+        }
+      }
+      return IPs;
+    }
+//    for (int i = 0; i < lines.size() ; i++) {
+//      IPAddress = lines.get(i).split("   ")[1];
+//      if (!uniqueIPs.containsKey(IPAddress)) {
+//        uniqueIPs.put(IPAddress, 1);
+//      } else {
+//        //Integer temp = uniqueIPs.get(IPAddress);
+//        uniqueIPs.put(IPAddress, uniqueIPs.get(IPAddress) + 1);
+//      }
+//    }
+//    for (String everyUniqueIp : uniqueIPs.keySet()) {
+//      IPAddress = everyUniqueIp;
+//
+//    }
+//    return IPAddress;
+//  }
 }
