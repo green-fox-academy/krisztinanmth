d366d6737ba770ea4d3a624ce253f3dff7b65ffd
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-30 13:19:12 +0200

    doubled text decryption exercise finished

---
+++ week-05/HeadFirstJava/src/Doubled.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Doubled {
+
+  public static void main(String[] args) {
+    // Create a method that decrypts the duplicated-chars.txt
+    pleaseDecryptText();
+  }
+
+  public static void pleaseDecryptText() {
+    try {
+      Path filePath = Paths.get("assets/doubled.txt");
+      List<String>lines = Files.readAllLines(filePath);
+      for (int i = 0; i < lines.size() ; i++) {
+        for (int j = 0; j < lines.get(i).length(); j += 2) {
+          System.out.print(lines.get(i).charAt(j));
+        }
+        System.out.println();
+      }
+    } catch (Exception e) {
+      System.out.println("file is not found or not readable, sorry");
+    }
+  }
+}
+
+
