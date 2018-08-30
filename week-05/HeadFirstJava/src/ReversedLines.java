c8beee34b2f3784b8c6ae16ccc4f47fb47f5f4a2
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-08-30 13:31:05 +0200

    reversedLines file exercise finished

--- week-05/HeadFirstJava/src/ReversedLines.java
+++ week-05/HeadFirstJava/src/ReversedLines.java
@@ -7,6 +7,7 @@ public class ReversedLines {
 
   public static void main(String[] args) {
     // Create a method that decrypts reversed-lines.txt
+    pleaseReverseLines();
 
   }
 
@@ -15,7 +16,7 @@ public class ReversedLines {
       Path filePath = Paths.get("assets/reversed.txt");
       List<String> lines = Files.readAllLines(filePath);
       for (int i = 0; i < lines.size() ; i++) {
-        for (int j = 0; j < lines.get(i).length(); j += 2) {
+        for (int j = lines.get(i).length() - 1; j >= 0; j--) {
           System.out.print(lines.get(i).charAt(j));
         }
         System.out.println();
