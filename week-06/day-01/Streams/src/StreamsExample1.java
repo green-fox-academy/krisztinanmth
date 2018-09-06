464a8b6ca7ea6c3b15fbd79ebff60e28e4b02bd7
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-09-06 13:59:47 +0200

    streams updated with more examples...though last one not working

--- week-06/day-01/Streams/src/StreamsExample1.java
+++ week-06/day-01/Streams/src/StreamsExample1.java
@@ -1,5 +1,8 @@
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamsExample1 {
@@ -30,15 +33,94 @@ public class StreamsExample1 {
 //    Stream<Person> stream = people.stream();
 
 
-    long count = people.stream()
+    long counter = people.stream()
       //inside the stream we have the lambda expression(s) as below
       .filter(p -> p.getFirstName().startsWith("c"))
       .count();
-    System.out.println(count );
+    System.out.println(counter);
 
     //or we can also do parallel streaming
     //        people.parallelStream()
     //look into it
+
+    //generating streams ...
+    //.stream() - returns a sequential stream considering collection as its source
+
+    //.parallelStream() - returns a parallel stream considering collection as its source
+
+
+//      stream has provided a new method ‘forEach’ to iterate each element of the stream.
+//      the following code segment shows how to print 3 random numbers using forEach.
+
+    Random random = new Random();
+    System.out.println();
+    System.out.println("forEach method: ");
+    random.ints().limit(3).forEach(System.out::println);
+
+
+//      the ‘map’ method is used to map each element to its corresponding result.
+//      the following code segment prints unique squares of numbers using map.
+
+    List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
+    //get list of unique squares
+    List<Integer> squaresList = numbers.stream().map( i -> i * i).distinct().collect(Collectors.toList());
+    System.out.println();
+    System.out.println("map method: ");
+    System.out.println(squaresList);
+
+
+  //    the ‘limit’ method is used to reduce the size of the stream.
+  //    the following code segment shows how to print 2 random numbers using limit.
+
+    Random random2 = new Random();
+    System.out.println();
+    System.out.println("limit method: ");
+    random2.ints().limit(2).forEach(System.out::println);
+
+
+//      the ‘sorted’ method is used to sort the stream.
+//      the following code segment shows how to print 5 random numbers in a sorted order.
+
+    Random random3 = new Random();
+    System.out.println();
+    System.out.println("sorted method: ");
+    random3.ints().limit(5).sorted().forEach(System.out::println);
+
+
+//    parallelStream is the alternative of stream for parallel processing.
+//    take a look at the following code segment that prints a count of empty strings using parallelStream.
+
+    List<String> emptyStrings = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
+//get count of empty string
+    int count = (int) emptyStrings.parallelStream().filter(string -> string.isEmpty()).count();
+    System.out.println();
+    System.out.println("parallel processing: ");
+    System.out.println(count);
+
+
+//      collectors are used to combine the result of processing on the elements of a stream.
+//      collectors can be used to return a list or a string.
+
+    List<String>strings = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
+    List<String> filtered = strings.stream().filter(string -> !string.isEmpty()).collect(Collectors.toList());
+    System.out.println();
+    System.out.println("Filtered List: " + filtered);
+    String mergedString = strings.stream().filter(string -> !string.isEmpty()).collect(Collectors.joining(", "));
+    System.out.println("Merged String: " + mergedString);
+
+
+
+//    with Java 8, statistics collectors are introduced to calculate all statistics when
+//    stream processing is being done.
+
+//    List integers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
+//
+//    IntSummaryStatistics stats = integers.stream().mapToInt((x) -> x).summaryStatistics();
+//
+//    System.out.println("Highest number in List : " + stats.getMax());
+//    System.out.println("Lowest number in List : " + stats.getMin());
+//    System.out.println("Sum of all numbers : " + stats.getSum());
+//    System.out.println("Average of all numbers : " + stats.getAverage());
   }
 }
 
