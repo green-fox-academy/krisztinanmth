7198fc725f0ba2c640c4b74c2414bcb10fe7eb67
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-09-07 09:42:57 +0200

    animalShelter started - classes created

---
+++ week-05/AnimalShelterApp/src/AnimalShelterApp.java
@@ -0,0 +1,19 @@
+public class AnimalShelterApp {
+  /*
+  Hey guys! I’m sorry that I haven’t sent you OO practice exercise yet.
+1. If you haven’t solved the Garden application by yourself, do that.
+2. If you haven’t solved the Aircraft carrier exercise by yourself, do that
+3. If you haven’t solved the Pirate exercise till you can fight with ships, do that
+4. If you done all of that (you shouldn't care about the exam ;)), create an animal shelter application where
+- animals can be stored in a list
+- animals can be adopted by people
+- one person can adopt many animals, but one can be adopted by only one person
+- if an animal is adopted, it is no longer listed in the shelter, but belongs to the
+ adopter person, and the owner of the animal is also stored in the animal object
+
+I’ll have some free time tomorrow afternoon, you can find me in GF. Contact me if you’d like me to help in anything then.
+   */
+  public static void main(String[] args) {
+
+  }
+}
