81369fce33b79e07ccd7de1b718c52a7a778466a
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-09-08 13:23:11 +0200

    exercise5 finished both with stream and method working

--- week-06/day-01/src/Exercise5.java
+++ week-06/day-01/src/Exercise5.java
@@ -6,12 +6,27 @@ import java.util.stream.Collectors;
 public class Exercise5 {
 
   public static void main(String[] args) {
-//    write a stream expression to find the uppercase characters in a string!
+    /**
+     * write a stream expression to find the uppercase characters in a string!
+     */
     String upperCase = "let me DO the BEST of it".chars()
       .filter(Character::isUpperCase)
       .mapToObj(c -> Character.toString((char)c))
       .collect(Collectors.joining());
     System.out.println("upperCases found with stream: ");
     System.out.println(upperCase);
+
+    String example = "let me DO the BEST of it";
+    System.out.println();
+    System.out.println("upperCases found with method: ");
+    findUpperCaseChars(example);
+  }
+
+  private static void findUpperCaseChars(String example) {
+    for (char c : example.toCharArray()) {
+      if (Character.isUpperCase(c)) {
+        System.out.print(c);
+      }
+    }
   }
 }
