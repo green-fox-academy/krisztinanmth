4c88658f89ff3ceae4192d79a8fb332724fa63d0
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-09-08 15:17:02 +0200

    Exercise7 done with stream expression, and method ... working

---
+++ week-06/day-01/src/Exercise7.java
@@ -0,0 +1,30 @@
+public class Exercise7 {
+
+  public static void main(String[] args) {
+    /**
+     * write a stream expression to find the frequency of characters in a given string!
+     */
+    String example = "peetweet";
+    long count = example.chars().filter(ch -> ch == 'e').count();
+    long count2 = example.chars().filter(ch -> ch == 't').count();
+    System.out.println("frequency of 'e' with stream: ");
+    System.out.println(count);
+    System.out.println("frequency of 't' with stream: ");
+    System.out.println(count2);
+
+    System.out.println("frequency of 'e' with method: ");
+    System.out.println(countOccurence(example, 'e'));
+    System.out.println("frequency of 't' with method: ");
+    System.out.println(countOccurence(example, 't'));
+  }
+
+  private static int countOccurence(String example, char searchedChar) {
+    int counter = 0;
+    for (int i = 0; i < example.length() ; i++) {
+      if (example.charAt(i) == searchedChar) {
+        counter++;
+      }
+    }
+    return counter;
+  }
+}
