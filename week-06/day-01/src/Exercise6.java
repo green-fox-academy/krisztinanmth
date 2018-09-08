2193e3e73250893d5880f2fe3b4e51c0954370e8
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-09-08 13:31:03 +0200

    Exercise6 done with stream expression, working

---
+++ week-06/day-01/src/Exercise6.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Exercise6 {
+
+  public static void main(String[] args) {
+    /**
+     * write a stream expression to find the strings which starts with A and ends with I in the following array:
+     */
+    ArrayList<String> cities = new ArrayList<>(Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS"));
+
+    List<String> citiesStartsAEndsI =
+      cities.stream()
+      .filter(s -> s.startsWith("A") && s.endsWith("I"))
+      .collect(Collectors.toList());
+    System.out.println("cities, which start with A & end with I, found with stream");
+    System.out.println(citiesStartsAEndsI);
+  }
+}
