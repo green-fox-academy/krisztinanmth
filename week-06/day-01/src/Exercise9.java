9e7f8eae9c827e26c409bb61a880a50ba981d1bd
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-09-09 14:42:41 +0200

    method good working ...stream is done with Character List and not char[]...so need to come back to this one

---
+++ week-06/day-01/src/Exercise9.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Exercise9 {
+
+  public static void main(String[] args) {
+    /**
+     *  write a stream expression to convert a char array to a string!
+     */
+    String str = "this is a very good day, enjoy";
+    char[] strChars = str.toCharArray();
+
+    //built in method for printing elements of char[] ...below
+    System.out.println(Arrays.toString(strChars));
+    //built in method for displaying char[] as a String ... below
+    System.out.println(String.valueOf(strChars));
+    System.out.println();
+
+    List<Character> chars = Arrays.asList('g', 'i', 't', 'H', 'u', 'b');
+    System.out.println(chars);
+    String joinedCharsStr = chars.stream()
+                                 .map(String::valueOf)
+                                 .collect(Collectors.joining());
+    System.out.println("turning char array into string with stream expression: ");
+    System.out.println(joinedCharsStr);
+    System.out.println();
+    System.out.println("turning char array into string with method: ");
+    System.out.println(turnMyCharArray(strChars));
+  }
+
+  private static String turnMyCharArray(char[] example) {
+    String temp = "";
+    for (char c : example) {
+      temp += c;
+    }
+    return temp;
+  }
+
+//  private static String joinCharArray(char[] strChars) {
+//    return (String) Arrays.asList(strChars)
+//      .stream()
+//      .collect(Collectors.joining());
+//  }
+}
