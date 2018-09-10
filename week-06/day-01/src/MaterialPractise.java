17fbf34ff6a43ec4438e7cdd366e3df33e7ad1aa
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-09-10 16:24:31 +0200

    material practise with kond updated

--- week-06/day-01/src/MaterialPractise.java
+++ week-06/day-01/src/MaterialPractise.java
@@ -4,31 +4,57 @@ import java.util.stream.Collectors;
 
 public class MaterialPractise {
 
+  public static int increaseAndPrint(int number) {
+    return ++number;
+  }
+
   public static void main(String[] args) {
-    int[] numberArray = new int[]{1, 2, 3, 4, 5, 6};
-    List<Integer> numberList = Arrays.asList(12, 12, 23, 37, 64);
+
+    int[] numberArray = new int[]{ 1, 2, 3, 4, 5, 6 };
+    List<Integer> numberList = Arrays.asList(10, 12, 23, 64, 37);
 
     double listAverage = numberList.stream()
-      //first i need to turn it into int stream with map and then i can use the .average on it
       .mapToInt(n -> n)
       .average()
       .getAsDouble();
     System.out.println(listAverage);
 
     double arrayAverage = Arrays.stream(numberArray)
-      //this already turned my List into an int stream
       .average()
       .getAsDouble();
     System.out.println(arrayAverage);
 
-    System.out.println(getOddElementsAverage(numberList));
+
+    double oddNumbers = getOddElements(numberList);
+    System.out.println(oddNumbers);
 
     int sum = sum(numberArray);
     System.out.println(sum);
 
     String text = "apple";
-    String encodedText = ceasarEncode(text, 2);
+    String encodedText = ceasarEncode(text, 200);
     System.out.println(encodedText);
+
+    printAll(numberArray);
+
+    User user1 = new User(1);
+    User user2 = new User(13);
+    List<User> users = Arrays.asList(user1, user2);
+
+    incrementUserIds(users);
+    System.out.println(users);
+
+  }
+
+  private static void incrementUserIds(List<User> users) {
+    users.stream().forEach(user -> user.incerementId());
+  }
+
+  private static void printAll(int[] numberArray) {
+    int[] incrementedNumbers = Arrays.stream(numberArray)
+      .map(MaterialPractise::increaseAndPrint)
+      .toArray();
+    System.out.println(Arrays.toString(incrementedNumbers));
   }
 
   private static String ceasarEncode(String text, int offset) {
@@ -37,25 +63,19 @@ public class MaterialPractise {
       .mapToObj(c -> Character.toString((char) c))
       .collect(Collectors.joining());
   }
-  //predicates are those lambda expressions which return with a boolean
 
-    private static double getOddElementsAverage(List<Integer> numberList) {
-      return numberList.stream()
-        .filter(n -> n % 2 != 0)
-        .mapToInt(n -> n)
-        .average()
-        .getAsDouble();
-    }
-
-    private static int sum(int[] numberArray) {
+  private static int sum(int[] numberArray) {
     return Arrays.stream(numberArray)
       .reduce((n1, n2) -> n1 + n2)
       .getAsInt();
+  }
 
-    //so the reducer works like this 1, 2, 3, 4
-      //(n1, n2) -> n1 + n2
-      //3, 3, 4
-      //6, 4
-      //10
-    }
+  private static double getOddElements(List<Integer> numberList) {
+    return numberList.stream().parallel()
+      .filter(n -> n % 2 != 0)
+      .mapToInt(n -> n)
+      .average()
+      .getAsDouble();
+  }
+
 }
