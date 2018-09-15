2a7af69fc4b7c10d4b4c5e6acefc0c88c9e211f2
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-09-15 15:39:56 +0200

    feat(rereTakeExam): just thought doing it for practising, multiplication table done

--- week-06/day-01/src/Exercise10.java
+++ week-06/day-01/src/Exercise10.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
 import static java.util.stream.Collectors.groupingBy;
 
 public class Exercise10 {
--- week-06/day-05/RereTakeExamPractise/src/MultiplicationTable.java
+++ week-06/day-05/RereTakeExamPractise/src/MultiplicationTable.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+/**
+ *  Create a function that prints the multiplication table of a given base number.
+ *  It should take the base number as a parameter and prints the output to the
+ *  console.
+ *
+ *  The output should look like this for 5 as base:
+ *   1 * 5 = 5
+ *   2 * 5 = 10
+ *   3 * 5 = 15
+ *   4 * 5 = 20
+ *   5 * 5 = 25
+ *   6 * 5 = 30
+ *   7 * 5 = 35
+ *   8 * 5 = 40
+ *   9 * 5 = 45
+ *   10 * 5 = 50
+ */
 
 public class MultiplicationTable {
+  public static void main(String[] args) {
+
+    multiplyInputNumber(5);
+    multiplyInputNumber(0);
+    multiplyInputNumber(3);
+    multiplyInputNumber(-8);
+  }
+
+  public static void multiplyInputNumber(int inputNumber) {
+    int multipliedNumber;
+    for (int i = 1; i <= 10 ; i++) {
+      multipliedNumber = i * inputNumber;
+      System.out.println(String.format("%d * %d = %d",i, inputNumber, multipliedNumber));
+    }
+  }
 }
