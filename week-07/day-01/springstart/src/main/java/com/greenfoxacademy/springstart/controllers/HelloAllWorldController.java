dcd1048f7c58930ac76d9d53669979c31ed8f818
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-09-17 20:40:57 +0200

    feat(HelloAllWorld): added, but not working just yet

---
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloAllWorldController.java
@@ -0,0 +1,24 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import com.greenfoxacademy.springstart.models.HelloAllWorld;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Random;
+
+@Controller
+public class HelloAllWorldController {
+
+  @GetMapping("/helloallworld")
+  public String greetTheWorld(Model model, @RequestParam("name") String name) {
+    Random random = new Random();
+    int randomColor = random.nextInt(256);
+    HelloAllWorld randomHello = new HelloAllWorld();
+    model.addAttribute("name", name);
+    model.addAttribute("helloallworld", randomHello.printRandomHello());
+    model.addAttribute("color", randomColor + ", " + randomColor + ", " + randomColor);
+    return "helloallworld";
+  }
+}
