7fd24d21774e9351c7587f8aaaf0975815b55097
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-09-17 17:48:52 +0200

    feat(springstart): web greet counter app finished and working

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -4,13 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @Controller
 public class HelloWebController {
+  AtomicLong id;
+
+  public HelloWebController() {
+    id = new AtomicLong(1);
+  }
 
   @RequestMapping("/web/greeting")
   public String greeting(Model model) {
     model.addAttribute("name", " World");
+    model.addAttribute("id", id.getAndIncrement());
     return "greeting";
   }
-
 }
