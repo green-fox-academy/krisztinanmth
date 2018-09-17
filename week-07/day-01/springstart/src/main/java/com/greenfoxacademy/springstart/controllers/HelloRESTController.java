f991681945dbb35027b4be302bd70e5329b7c1aa
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-09-17 15:31:36 +0200

    feat(Hello User REST App): finished & working

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,16 +1,20 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import com.greenfoxacademy.springstart.models.Greetings;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloRESTController {
 
 
-  @GetMapping("/greeting")
-  public Greetings greeting() {
-    Greetings greeting = new Greetings(1, "hello");
+  @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+  public Greetings greeting(@RequestParam(value = "name") String name) {
+    Greetings greeting = new Greetings(1, "hello " + name);
     return greeting;
   }
+
+  /**
+   * Open http://localhost:8080/greeting?name=Your%20name in your web browser
+   * Your output should look like: {"id":1,"content":"Hello, Your name!"}
+   */
 }
