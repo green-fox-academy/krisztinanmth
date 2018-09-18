4d8c9b593613c55f3eda28a1ce107e12df00c4e7
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-09-18 16:21:15 +0200

    feat(BankOfSimba): filling and showing all bank accounts in list finished & working

--- week-07/day-02/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/controllers/BankAccountController.java
+++ week-07/day-02/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/controllers/BankAccountController.java
@@ -5,8 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class BankAccountController {
+
 
   @GetMapping("/show")
   public String showBankAccount(Model model) {
@@ -26,6 +31,17 @@ public class BankAccountController {
     String message = "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>";
     model.addAttribute("enjoyYourself", message);
     return "enjoyYourself";
+  }
+
+  @GetMapping("/show/list")
+  public String showListOfBankAccounts(Model model) {
+    List<BankAccount> allBankAccounts;
+    allBankAccounts = Arrays.asList(new BankAccount("simba", 4000, "lion"),
+      new BankAccount("nala", 600, "lion"),
+      new BankAccount("zazu", 1000, "bird"),
+      new BankAccount("banzai", 2300, "something"));
+    model.addAttribute("bankList", allBankAccounts);
+    return "show-allBankAccounts";
   }
 
 }
