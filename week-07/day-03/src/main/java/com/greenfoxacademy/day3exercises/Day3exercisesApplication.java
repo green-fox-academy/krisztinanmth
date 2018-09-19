997cf3c55c8a2d611677f51832217aeb9593272f
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-09-19 12:56:32 +0200

    chore(HelloBeanWorldApp): started

---
+++ week-07/day-03/src/main/java/com/greenfoxacademy/day3exercises/Day3exercisesApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.day3exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3exercisesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Day3exercisesApplication.class, args);
+  }
+}
---
+++ week-07/day-03/src/test/java/com/greenfoxacademy/day3exercises/Day3exercisesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.day3exercises;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Day3exercisesApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
