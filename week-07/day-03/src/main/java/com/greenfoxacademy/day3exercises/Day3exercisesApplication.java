47762f9f02ea9371dce35244df193c5b5d53a232
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-09-19 13:08:01 +0200

    feat(Hello DI Project): finished & working

--- week-07/day-03/src/main/java/com/greenfoxacademy/day3exercises/Day3exercisesApplication.java
+++ week-07/day-03/src/main/java/com/greenfoxacademy/day3exercises/Day3exercisesApplication.java
@@ -1,12 +1,25 @@
 package com.greenfoxacademy.day3exercises;
 
+import com.greenfoxacademy.day3exercises.service.Printer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Day3exercisesApplication {
+public class Day3exercisesApplication implements CommandLineRunner {
+
+  @Autowired
+  Printer printer;
 
   public static void main(String[] args) {
     SpringApplication.run(Day3exercisesApplication.class, args);
   }
+
+  @Override
+  public void run(String... args) throws Exception {
+    printer.log("hello bello");
+  }
 }
+
+
