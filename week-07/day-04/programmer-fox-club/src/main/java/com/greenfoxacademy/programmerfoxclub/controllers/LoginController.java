62b9eaa20085204faee21db3b93b4e6ce19d8fca
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-09-20 19:14:14 +0200

    chore(programmer-fox-club): just trying out sg

--- week-07/day-04/programmer-fox-club/src/main/java/com/greenfoxacademy/programmerfoxclub/controllers/LoginController.java
+++ week-07/day-04/programmer-fox-club/src/main/java/com/greenfoxacademy/programmerfoxclub/controllers/LoginController.java
@@ -1,33 +1,58 @@
 package com.greenfoxacademy.programmerfoxclub.controllers;
 
+import com.greenfoxacademy.programmerfoxclub.service.FoxService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-  @GetMapping("/")
-  public String getIndex(@RequestParam(value="name", required = false) String name, Model model) {
-    if (name == null) {
-      return "redirect:/login";
-    }
-    model.addAttribute("name", name);
-    return "index";
+//  @GetMapping("/")
+//  public String getIndex(@RequestParam(value="name", required = false) String name, Model model) {
+//    if (name == null) {
+//      return "redirect:/login";
+//    }
+//    model.addAttribute("name", name);
+//    return "index";
+//  }
+//
+//  @GetMapping("/login")
+//  public String getLogin() {
+//    return "login";
+//  }
+//
+//  @PostMapping("/login")
+//  public String createNameOfPet(@RequestParam("name") String name) {
+//    if (name == null || name == "") {
+//      return "redirect:/login";
+//    }
+//    return "redirect:/?name=" + name;
+//  }
+
+  private FoxService foxService;
+
+  public LoginController() {
+  }
+
+  public LoginController(FoxService foxService) {
+    this.foxService = foxService;
   }
 
   @GetMapping("/login")
-  public String getLogin() {
+  public String showLoginPage() {
     return "login";
   }
 
-  @PostMapping("/login")
-  public String createNameOfPet(@RequestParam("name") String name) {
+  @GetMapping("")
+  public String login(@RequestParam(value = "name", required = false) String name, Model model) {
     if (name == null || name == "") {
       return "redirect:/login";
     }
-    return "redirect:/?name=" + name;
+    foxService.addNewFoxToList(name);
+    model.addAttribute("name", name);
+    model.addAttribute("fox", foxService.getFox(name));
+    return "index";
   }
 }
