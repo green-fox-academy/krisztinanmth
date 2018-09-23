4fd1ea72048f019c86dba0aa863e9bdaaa257f58
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-09-23 10:16:41 +0200

    chore(ShelterApp): initiate project

--- week-08/day-01/ShelterAppWithKond/src/main/java/com/greenfoxacademy/shelterapp/ShelterappApplication.java
+++ week-08/day-01/ShelterAppWithKond/src/main/java/com/greenfoxacademy/shelterapp/ShelterappApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShelterappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShelterappApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ShelterappApplication.class, args);
+  }
 }
--- week-08/day-01/ShelterAppWithKond/src/test/java/com/greenfoxacademy/shelterapp/ShelterappApplicationTests.java
+++ week-08/day-01/ShelterAppWithKond/src/test/java/com/greenfoxacademy/shelterapp/ShelterappApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ShelterappApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Test
+  public void contextLoads() {
+  }
 
 }
