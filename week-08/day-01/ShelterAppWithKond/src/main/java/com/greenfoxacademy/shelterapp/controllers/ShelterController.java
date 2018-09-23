26d2043eb67c14a2b41a8786c4e45a5cbd0bd5d6
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-09-23 10:48:24 +0200

    feat(Shelters): create Shelter model & increment GET/endpoint

---
+++ week-08/day-01/ShelterAppWithKond/src/main/java/com/greenfoxacademy/shelterapp/controllers/ShelterController.java
@@ -0,0 +1,21 @@
+package com.greenfoxacademy.shelterapp.controllers;
+
+import com.greenfoxacademy.shelterapp.models.Shelter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class ShelterController {
+
+  @GetMapping("")
+  public String showSheltersPage(Model model) {
+    Shelter shelter = new Shelter("krisztina's shelter");
+    List<Shelter> shelters = Arrays.asList(shelter);
+    model.addAttribute("shelters", shelters);
+    return "shelters";
+  }
+}
---
+++ week-08/day-01/ShelterAppWithKond/src/main/java/com/greenfoxacademy/shelterapp/models/Shelter.java
@@ -0,0 +1,54 @@
+package com.greenfoxacademy.shelterapp.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Shelter {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+  private String name;
+
+  public Shelter() {
+
+  }
+
+  public Shelter(String name) {
+    this.name = name;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
