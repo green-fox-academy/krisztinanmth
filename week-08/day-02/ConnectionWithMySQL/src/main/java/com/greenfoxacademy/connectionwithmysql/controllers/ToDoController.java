23399b6d29378df637ff7a1a5f364ecfe0127144
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-09-25 17:36:44 +0200

    feat(ConnectionWithMySQL): add creating function

--- week-08/day-02/ConnectionWithMySQL/src/main/java/com/greenfoxacademy/connectionwithmysql/controllers/ToDoController.java
+++ week-08/day-02/ConnectionWithMySQL/src/main/java/com/greenfoxacademy/connectionwithmysql/controllers/ToDoController.java
@@ -1,10 +1,12 @@
 package com.greenfoxacademy.connectionwithmysql.controllers;
 
+import com.greenfoxacademy.connectionwithmysql.models.ToDo;
 import com.greenfoxacademy.connectionwithmysql.repositories.ToDoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -30,5 +32,16 @@ public class ToDoController {
       model.addAttribute("todolist", toDoRepository.findByDone(done));
     }
     return "todolist";
+  }
+
+  @GetMapping("/add")
+  public String addToDo() {
+    return "create-todo";
+  }
+
+  @PostMapping("/add")
+  public String postToDo(@RequestParam(value = "title") String title) {
+    toDoRepository.save(new ToDo(title));
+    return "redirect:/todo/";
   }
 }
