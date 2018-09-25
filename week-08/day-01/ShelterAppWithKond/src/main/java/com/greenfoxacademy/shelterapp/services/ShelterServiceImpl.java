2b738201fc99756245c73e9186f51f7cc4d85256
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-09-25 12:51:09 +0200

    chore(ToDo): create new project

--- week-08/day-01/ShelterAppWithKond/src/main/java/com/greenfoxacademy/shelterapp/services/ShelterServiceImpl.java
+++ week-08/day-01/ShelterAppWithKond/src/main/java/com/greenfoxacademy/shelterapp/services/ShelterServiceImpl.java
@@ -3,9 +3,11 @@ package com.greenfoxacademy.shelterapp.services;
 import com.greenfoxacademy.shelterapp.models.Shelter;
 import com.greenfoxacademy.shelterapp.repositories.ShelterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ShelterServiceImpl implements ShelterService {
 
   ShelterRepository shelterRepository;
