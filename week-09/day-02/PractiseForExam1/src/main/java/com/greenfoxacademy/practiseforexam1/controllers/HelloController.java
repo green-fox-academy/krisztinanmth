915384d56552ae490890a2216939d62bef754aeb
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-10-02 14:50:02 +0200

    feat(HelloController): actually this layout fragment thing is not working

---
+++ week-09/day-02/PractiseForExam1/src/main/java/com/greenfoxacademy/practiseforexam1/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.practiseforexam1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+  @GetMapping("/hello")
+  public String showHello() {
+    return "hello";
+  }
+}
