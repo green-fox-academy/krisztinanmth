3b61c9b6c41e19c567b9101a8dc78cf205b8606a
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-10-02 14:54:30 +0200

    chore(ShelterApp): init project

---
+++ week-09/day-02/ShelterApp/src/main/java/com/greenfoxacademy/shelterapp/ShelterappApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.shelterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShelterappApplication.class, args);
+  }
+}
