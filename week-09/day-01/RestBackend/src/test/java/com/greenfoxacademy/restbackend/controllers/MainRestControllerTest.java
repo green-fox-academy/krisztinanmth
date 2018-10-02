bf765cead2029d1746c5bfe0d2439bd19caf1ec3
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-10-02 13:11:41 +0200

    test(doublingShouldReturnProperResult_when_inputIsGiven): create test when there is input

---
+++ week-09/day-01/RestBackend/src/test/java/com/greenfoxacademy/restbackend/controllers/MainRestControllerTest.java
@@ -0,0 +1,51 @@
+package com.greenfoxacademy.restbackend.controllers;
+
+import com.greenfoxacademy.restbackend.models.Doubling;
+import com.greenfoxacademy.restbackend.services.RestService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import java.nio.charset.Charset;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(MainRestController.class)
+public class MainRestControllerTest {
+
+  private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+    MediaType.APPLICATION_JSON.getSubtype(),
+    Charset.forName("utf8"));
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private RestService restService;
+
+  @Test
+  public void doublingShouldReturnProperResult_when_inputIsGiven() throws Exception {
+    int input = 21;
+    int result = 42;
+
+    when(restService.doubleTheInput(input)).thenReturn(new Doubling(input, result));
+
+    mockMvc.perform(get("/doubling?input=" + input))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(contentType))
+            .andExpect(jsonPath("$.received", is(input)))
+            .andExpect(jsonPath("$.result", is(result)))
+            .andDo(print());
+  }
+
+  
+}
