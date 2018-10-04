9f7e97a890d9b3db4d7be9eb09d44c935c4cb0b9
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-10-04 14:36:24 +0200

    feat(models): create Spaceship and Planet classes

--- week-09/day-04/Spaceship/src/main/java/com/greenfoxacademy/spaceship/models/Spaceship.java
+++ week-09/day-04/Spaceship/src/main/java/com/greenfoxacademy/spaceship/models/Spaceship.java
@@ -1,4 +1,57 @@
 package com.greenfoxacademy.spaceship.models;
 
-public class SpaceShip {
+import javax.persistence.*;
+
+
+@Entity
+public class Spaceship {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  @Column(name = "max_capacity")
+  private Integer maxCapacity;
+  @Column(name = "planet")
+  private String planetName;
+  private Integer utilization;
+
+//  public Spaceship() {
+//  }
+//
+//  public Spaceship(String planetName) {
+//    this.planetName = planetName;
+//  }
+
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Integer getMaxCapacity() {
+    return maxCapacity;
+  }
+
+  public void setMaxCapacity(Integer maxCapacity) {
+    this.maxCapacity = maxCapacity;
+  }
+
+  public String getPlanetName() {
+    return planetName;
+  }
+
+  public void setPlanetName(String planetName) {
+    this.planetName = planetName;
+  }
+
+  public Integer getUtilization() {
+    return utilization;
+  }
+
+  public void setUtilization(Integer utilization) {
+    this.utilization = utilization;
+  }
 }
