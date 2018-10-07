540ebd206609f1eec82593d9d8b74bd8b0fecd6f
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-10-07 09:13:53 +0200

    feat(AttractionsController): implement 1st endpoint GET/ with table of attractions in attractions.html

--- week-10/day-01/AlopexOrientationExamPractise/src/main/java/com/greenfoxacademy/alopexorientationexampractise/controllers/AttractionsController.java
+++ week-10/day-01/AlopexOrientationExamPractise/src/main/java/com/greenfoxacademy/alopexorientationexampractise/controllers/AttractionsController.java
@@ -1,4 +1,24 @@
 package com.greenfoxacademy.alopexorientationexampractise.controllers;
 
+import com.greenfoxacademy.alopexorientationexampractise.services.AttractionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AttractionsController {
+
+  private AttractionService attractionService;
+
+  @Autowired
+  public AttractionsController(AttractionService attractionService) {
+    this.attractionService = attractionService;
+  }
+
+  @GetMapping("/")
+  public String showAttractionsPage(Model model) {
+    model.addAttribute("attractions", attractionService.getAllAttractions());
+    return "attractions";
+  }
 }
--- week-10/day-01/AlopexOrientationExamPractise/src/main/java/com/greenfoxacademy/alopexorientationexampractise/repositories/AttractionRepository.java
+++ week-10/day-01/AlopexOrientationExamPractise/src/main/java/com/greenfoxacademy/alopexorientationexampractise/repositories/AttractionRepository.java
@@ -3,9 +3,11 @@ package com.greenfoxacademy.alopexorientationexampractise.repositories;
 import com.greenfoxacademy.alopexorientationexampractise.models.Attraction;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 
 @Repository
 public interface AttractionRepository extends CrudRepository<Attraction, Long> {
 
+  List<Attraction> findAll();
 }
