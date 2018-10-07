d3d957cd3a33c969ba38e28b9c17f60fdc5d7ebf
Krisztina Nemeth <nkrisztina82@gmail.com>
2018-10-07 08:25:30 +0200

    chore(AlopexOrientationExamPractise): init project

---
+++ week-10/day-01/AlopexOrientationExamPractise/src/main/java/com/greenfoxacademy/alopexorientationexampractise/AlopexorientationexampractiseApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.alopexorientationexampractise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlopexorientationexampractiseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AlopexorientationexampractiseApplication.class, args);
+  }
+}
