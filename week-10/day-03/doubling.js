16ee0bdb9da96d9e1c3c5ab186c7bc316b6a57fe
krisztina <nkrisztina82@gmail.com>
2018-10-10 16:48:33 +0200

    feat(doubling): create function that doubles input number

---
+++ week-10/day-03/doubling.js
@@ -0,0 +1,12 @@
+'use strict';
+// - Create a variable named `baseNum` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+// - Log the result of `doubling(baseNum)` to the console
+
+let baseNum = 123;
+
+function doubling(number) {
+    return number * 2;
+}
+
+console.log(doubling(baseNum));
