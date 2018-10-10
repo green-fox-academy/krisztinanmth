1056cc9162606db479787a5743cbebe08b0e03ba
krisztina <nkrisztina82@gmail.com>
2018-10-10 17:19:42 +0200

    feat(factorio): create factorio function

---
+++ week-10/day-03/factorial.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// - Create a function called `factorio`
+//   that returns it's input's factorial
+
+function factorio(number) {
+    let result = 1;
+    for (var i = 1; i <= number; i ++)
+    result *= i;
+    return result;
+}
+console.log(factorio(5));
