4fb0050b1e59f537a345f67f3cf4f8c990b0ad68
krisztina <nkrisztina82@gmail.com>
2018-10-11 12:43:34 +0200

    feat(unique.js): create function that returns unique array

---
+++ week-10/day-03/unique.js
@@ -0,0 +1,18 @@
+//  Create a function that takes a list of numbers as a parameter
+//  Returns a list of numbers where every number in the list occurs only once
+
+function unique(arr) {
+    let tempArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (tempArray.indexOf(arr[i]) === -1) { ///// hogy veszem ki i indexbol a szamot????????????????????????????????????????
+            tempArray.push(arr[i]);
+        }
+    }
+    return tempArray;
+}
+
+//  Example
+console.log(unique([1, 11, 34, 11, 52, 61, 1, 34]));
+//  should print: `[1, 11, 34, 52, 61]`
+
+
