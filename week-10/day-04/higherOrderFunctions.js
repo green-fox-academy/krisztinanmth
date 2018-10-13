7eef0081a7c9daec782b24f5929a52680f875e49
krisztina <nkrisztina82@gmail.com>
2018-10-13 12:38:48 +0200

    feat(higherOrderFunctions.js): add examples

--- week-10/day-04/higherOrderFunctions.js
+++ week-10/day-04/higherOrderFunctions.js
@@ -50,6 +50,16 @@ function repeat(n, action) {
   console.log(reduce([1, 2, 3, 4], myCombine , 0));
   // → 10
   // or console.log(reduce([1, 2, 3, 4], (a, b) => a + b, 0)); - this is the same as line 51 :)
-
   const myMultiply = (n1, n2) => n1 * n2;
   console.log(reduce([3, 5, 7, 1], myMultiply, 1));
+
+  // Fibonacci fv: az argumentum az elem sorszáma, a visszatérési érték maga az elem:
+// var fibonacci = function(n) {
+//     if (n < 2) {
+//       return n;
+//     } else {
+//       return fibonacci(n - 1) + fibonacci(n - 2);
+//     }
+//   }
+//   for (var i = 0; i < 100; i++) console.log(fibonacci(i) + ', ');
+//ez nem jo....nezd at
