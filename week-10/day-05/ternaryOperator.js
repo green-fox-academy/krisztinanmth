8bcbb51bc3da222cde50c3ed4c5e92e28204cae6
krisztina <nkrisztina82@gmail.com>
2018-10-13 17:26:15 +0200

    feat(ternaryOperator.js): use multiple ternary operators in one function

--- week-10/day-05/ternaryOperator.js
+++ week-10/day-05/ternaryOperator.js
@@ -37,3 +37,40 @@ function checkEqual(a, b) {
   console.log(checkEqual(-1, 1));
   console.log(checkEqual(-1, 3));
   console.log(checkEqual(-1, -1));
+
+/*
+In the previous challenge, you used a single conditional operator. 
+You can also chain them together to check for multiple conditions.
+
+The following function uses if, else if, and else statements to 
+check multiple conditions:
+
+                    function findGreaterOrEqual(a, b) {
+                    if(a === b) {
+                        return "a and b are equal";
+                    }
+                    else if(a > b) {
+                        return "a is greater";
+                    }
+                    else {
+                        return "b is greater";
+                    }
+                    }
+The above function can be re-written using multiple conditional operators:
+
+        function findGreaterOrEqual(a, b) {
+        return (a === b) ? "a and b are equal" : (a > b) ? "a is greater" : "b is greater";
+        }
+*/
+
+//Use multiple conditional operators in the checkSign function 
+//to check if a number is positive, negative or zero.
+
+function checkSign(num) {
+    return (num < 0) ? "negative" : (num > 0) ? "positive" : "zero"
+  }
+  
+  console.log(checkSign(10));
+  console.log(checkSign(-3));
+  console.log(checkSign(0));
+  
