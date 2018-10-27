a82b82bb4a1988fb75b612ff627fc34ca6ec4f34
krisztina <nkrisztina82@gmail.com>
2018-10-27 16:16:08 +0200

    feat(multiplier.js): create multiplier functions, which can be used for doubling, tripling ..etc:

---
+++ JS-closures.callbacks.timers/multiplier.js
@@ -0,0 +1,18 @@
+'use strict'
+// Create the multiplier function that you can use like this:
+function multiplier(n) {
+  const multiplyThis = function(x) {
+    return n * x;
+  }
+  return multiplyThis;
+}
+
+const duplicator = multiplier(2);
+
+console.log(duplicator(5)); // should log 10
+console.log(duplicator(10)); // should log 20
+
+const threeTimes = multiplier(3);
+
+console.log(threeTimes(5)); // should log 15
+console.log(threeTimes(100)); // should log 300
