7e1ff237bd5995651c0e930abc864927fe023dcc
krisztina <nkrisztina82@gmail.com>
2018-11-12 13:54:05 +0100

    chore(todoListController.js): set up controller

---
+++ todoListApi/api/controllers/todoListController.js
@@ -0,0 +1,53 @@
+/*
+in this controller, we will be writing five(5) different functions namely:
+list_all_tasks, create_a_task, read_a_task, update_a_task, delete_a_task.
+we will export each of the functions to use in our routes.
+each of these functions uses different mongoose methods such as find, findById, findOneAndUpdate, save and remove.
+*/
+'use strict';
+
+const mongoose = require('mongoose'),
+  Task = mongoose.model('tasks');
+
+exports.list_all_tasks = function(req, res) {
+  Task.find({}, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+exports.create_a_task = function(req, res) {
+  const new_task = new Task(req.body);
+  new_task.save(function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+exports.read_a_task = function(req, res) {
+  Task.findById(req.params.taskId, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+exports.update_a_task = function(req, res) {
+  Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+exports.delete_a_task = function(req, res) {
+  Task.remove({
+    _id: req.params.taskId
+  }, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'task deleted successfully'});
+  });
+};
