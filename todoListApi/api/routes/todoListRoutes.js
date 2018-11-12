79e1d7079a1c6b5fcd4bc0e100df102b85922d24
krisztina <nkrisztina82@gmail.com>
2018-11-12 13:02:34 +0100

    chore(routes/): set up diff routes in todoListRoutes.js

---
+++ todoListApi/api/routes/todoListRoutes.js
@@ -0,0 +1,19 @@
+/*
+routing refers to determining how an application responds to a client request for a specific endpoint,
+which is a URI (or path) and a specific HTTP request method (GET, POST, and so on)...
+each of our routes has different route handler functions, which are executed when the route is matched.
+*/
+'use strict';
+module.exports = function(app) {
+  const todoList = this.require('../controllers/todoListController');
+
+  // todoList Routes
+  app.route('/tasks')
+    .get(todoList.list_all_tasks)
+    .post(todoList.create_a_task);
+
+  app.route('/tasks/:taskId')
+    .get(todoList.read_a_task)
+    .put(todoList.update_a_task)
+    .delete(todoList.delete_a_task);
+};
