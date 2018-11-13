3e51759f3ae9b619330655efc43698da2a7b902a
krisztina <nkrisztina82@gmail.com>
2018-11-13 21:12:13 +0100

    feat(server.js): connect handlers<controllers>, database, created models, body parser and created routes together

--- todoListApi/server.js
+++ todoListApi/server.js
@@ -1,7 +1,19 @@
 
-const express = require('express'),
-  app = express(),
+const express = require('express');
+  app = express();
   port = process.env.PORT || 3000;
+  mongoose = require('mongoose');
+  Task = require('./api/models/todoListModel'); // loading created model here
+  bodyParser = require('body-parser');
+
+// mongoose instance ... url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const routes = require('.api/routes/todoListRoutes'); // importing routes
 
 app.listen(port);
 
