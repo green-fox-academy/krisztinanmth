a353554901d91ac8882671462ab18ad0294eaaa0
krisztina <nkrisztina82@gmail.com>
2018-11-18 11:03:28 +0100

    feat(containers): build VisibleCounter container

---
+++ Redux-Practise/learn_redux/src/containers/VisibleCounter.js
@@ -0,0 +1,29 @@
+import { connect } from 'react-redux';
+import Counter from '../components/Counter';
+import * as actions from '../actions';
+
+// map Redux state to component props:
+function mapStateToProps(state) {
+  return {
+    count: state.CountReducer.count,
+    wish_value: state.CountReducer.wish_value
+  }
+}
+
+// map Redux actions to component props
+function mapDispatchToProps(dispatch) {
+  return {
+    onIncreaseClick: () => dispatch(actions.increasTodo()),
+    onUpdateClick: event => dispatch((actions.updateTodo(event.target.value)
+    )),
+  }
+}
+
+// connected Component
+const VisibleCounter = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
+
+
+export default VisibleCounter;
