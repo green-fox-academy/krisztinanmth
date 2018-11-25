5d220c9fc4f075c9bb2eb804a61de6d772304a90
krisztina <nkrisztina82@gmail.com>
2018-11-25 18:05:37 +0100

    chore(react_todoapp): add redux-persist to dependencies

--- react_todoapp/src/index.js
+++ react_todoapp/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+const store = compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)(rootReducer);
 
 render(
   <Provider store={store}>
