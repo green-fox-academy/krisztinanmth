ea5b0e70ad4094c11a702856613bf82e1a11b38c
krisztina <nkrisztina82@gmail.com>
2018-11-26 20:59:58 +0100

    feat(practise-persist): react-redux todoApp for practising with persisr

--- Redux-Practise/practise-persist/src/js/components/App.js
+++ Redux-Practise/practise-persist/src/js/components/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from '../store/index';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { store, persistor } from '../store/index';
 import List from './List';
 import Form from './Form';
 
 const App = () => (
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
       <div className="row mt-5">
         <div className="col-md-4 offset-md-1">
           <h2>Articles</h2>
@@ -18,7 +16,6 @@ const App = () => (
           <Form />
         </div>
       </div>
-    </PersistGate>
-  </Provider>
 );
+
 export default App;
--- Redux-Practise/practise-persist/src/js/store/index.js
+++ Redux-Practise/practise-persist/src/js/store/index.js
@@ -1,21 +1,21 @@
 import { createStore } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
 import rootReducer from '../reducers/index';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-};
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+// };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export default () => {
-  let store = createStore(persistedReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-  let persistor = persistStore(store);
-  return { store, persistor };
-};
+// export default () => {
+//   let store = createStore(persistedReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+//   let persistor = persistStore(store);
+//   return { store, persistor };
+// };
 
-// const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-// export default store;
+export default store;
