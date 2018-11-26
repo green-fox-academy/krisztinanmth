0d11bba7f47da77b74b76e2f7e53b32972e00544
krisztina <nkrisztina82@gmail.com>
2018-11-26 21:01:00 +0100

    feat(practise-persist): react-redux todoApp for practising with persisr

--- react_todoapp/src/index.js
+++ react_todoapp/src/index.js
@@ -2,14 +2,36 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore, compose } from 'redux';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './components/App';
 import rootReducer from './reducers';
+import persistor from './configureStore';
 
 const store = compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)(rootReducer);
 
 render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+      </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
+
+// import { createStore } from 'redux';
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web and AsyncStorage for react-native
+// import rootReducer from './reducers';
+
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+// };
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// export default () => {
+//   let store = createStore(persistedReducer)
+//   let persistor = persistStore(store)
+//   return { store, persistor }
+// };
