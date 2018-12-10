6452efb93d69974e93b4b28b91384fcb46278e40
krisztina <nkrisztina82@gmail.com>
2018-12-10 16:56:34 +0100

    del(todolistApi): delete todolistApi

---
+++ Hackerrank/Algorithms/TimeConcersion/src/com/greenfox/Main.java
@@ -0,0 +1,40 @@
+package com.greenfox;
+
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Main {
+
+    /*
+     * Complete the timeConversion function below.
+     */
+    static String timeConversion(String s) {
+
+      DateFormat dateFormat = new SimpleDateFormat("hh:mm:ssa");
+      Date result = new Date();
+      try {
+        result =  dateFormat.parse(s);
+      } catch (Exception e) { }
+      DateFormat dateFormatMilitary =  new SimpleDateFormat("HH:mm:ss", Locale.ENGLISH);
+      return (String) dateFormatMilitary.format(result);
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+      BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+      String s = scan.nextLine();
+
+      String result = timeConversion(s);
+
+      bw.write(result);
+      bw.newLine();
+
+      bw.close();
+    }
+
+}
