6b19d4658526eb3b12ef92243bcd84109baaeccc
krisztina <nkrisztina82@gmail.com>
2019-01-15 15:00:46 +0100

    feat(SquareNums.js): create function which uses built-in function map

---
+++ codecademy_projects/Built_inArrayFunctions/SquareNums.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const numbers = [2, 7, 9, 171, 52, 33, 14]
+
+const toSquare = num => num * num
+
+// Write your code here:
+const squareNums = array => array.map(e => e * e);
+
+console.log(squareNums(numbers));
