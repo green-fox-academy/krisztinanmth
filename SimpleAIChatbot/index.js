cf95611b680c9a223cdc52869dc0d23c38ad93d1
krisztina <nkrisztina82@gmail.com>
2019-01-17 12:59:00 +0100

    feat(index.js): instantiate Express and listen to the server

---
+++ SimpleAIChatbot/index.js
@@ -0,0 +1,13 @@
+// instantiate Express and listen to the server
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/views')); // html
+app.use(express.static(__dirname + '/public')); // css + js folders
+
+const server = app.listen(5000);
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
