d85c5128c3c791cc77a6d6c495b073249428adb8
krisztina <nkrisztina82@gmail.com>
2019-01-20 09:34:41 +0100

    add(Game.js): create Cell component...also i got lost :(

--- react-gameoflife/src/Game.js
+++ react-gameoflife/src/Game.js
@@ -5,6 +5,21 @@ const CELL_SIZE = 20;
 const WIDTH = 1500;
 const HEIGHT = 1000;
 
+class Cell extends React.Component {
+  render() {
+    const { x, y } = this.props;
+    return (
+      <div className="Cell" style={{
+        left: `${CELL_SIZE * x + 1}px`,
+        top: `${CELL_SIZE * y + 1}px`,
+        width: `${CELL_SIZE - 1}px`,
+        height: `${CELL_SIZE - 1}px`,
+      }}>
+      </div>
+    )
+  }
+}
+
 class Game extends React.Component {
   constructor() {
     super();
@@ -41,12 +56,43 @@ class Game extends React.Component {
     return cells;
   }
 
+  getElementOffset() {
+    const rect = this.boardRef.getBoundingClientRect();
+    const doc = document.documentElement;
+
+    return {
+      x: (rect.left + window.pageXOffset) - doc.clientLeft,
+      y: (rect.top + window.pageYOffset) - doc.clientTop,
+    };
+  }
+
+  handleClick = event => {
+    const elemOffSet = this.getElementOffset();
+    const offsetX = event.clientX - elemOffSet.x;
+    const offsetY = event.clientY - elemOffSet.y;
+
+    const x = Math.floor(offsetX / CELL_SIZE);
+    const y = Math.floor(offsetY / CELL_SIZE);
+
+    if (x >= 0 && x <= this.cols && y >= 0 && y <= this.rows) {
+      this.board[y][x] = !this.board[y][x];
+    }
+    this.setState({ cells: this.makeCells() });
+  }
+
   render() {
+    const { cells } = this.state;
     return (
       <div>
         <div className="Board"
           style={{ width: WIDTH, height: HEIGHT,
-          backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px` }}>
+          backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px` }}
+          onClick={this.handleClick}
+          ref={ (n) => { this.boardRef = n } }>
+          {cells.map(cell => (
+            <Cell x={cell.x} y={cell.y}
+              key={`${cell.x}, ${cell.y}`} />
+          ))}
         </div>
       </div>
     );
