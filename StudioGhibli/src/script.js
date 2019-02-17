76c8821f96b9dbb0f6de732049b0f6a7818dee51
krisztina <nkrisztina82@gmail.com>
2019-02-17 14:39:54 +0100

    feat(StudioGhibli): create script.js & style.css

--- StudioGhibli/src/script.js
+++ StudioGhibli/src/script.js
@@ -1,18 +1,41 @@
-// create a request variable and assign a new XMLHttpRequest object to it
-let request = new XMLHttpRequest();
+const app = document.getElementById('root');
 
-// open a new connection, using a GET request on the API's URL endpoint
+const logo = document.createElement('img');
+logo.src = 'logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
+const request = new XMLHttpRequest();
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.onload = function () {
 
-request.onload = () => {
-  // begin accessing JSON data here
-  let data = JSON.parse(this.response);
+  // Begin accessing JSON data here
+  const data = JSON.parse(this.response);
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
 
-  data.forEach(movie => {
-    // log each movie's title
-    console.log(movie.title);
-  });
+      const h1 = document.createElement('h1');
+      h1.textContent = movie.title;
+
+      const p = document.createElement('p');
+      movie.description = movie.description.substring(0, 300);
+      p.textContent = `${movie.description}...`;
+
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(p);
+    });
+  } else {
+    const errorMessage = document.createElement('marquee');
+    errorMessage.textContent = `Gah, it's not working!`;
+    app.appendChild(errorMessage);
+  }
 }
 
-// send request
 request.send();
